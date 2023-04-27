3f8428ded33161ef1fd6c2caf8ceef29e6d834d3
RuslanMirov <ruslan777mirov@gmail.com>
2023-04-27 12:13:43 +0300

    Update

--- router.js
+++ router.js
@@ -1,10 +1,41 @@
 require('dotenv').config()
 const express = require('express')
 const router = express.Router()
+const wordPerSocketDB = require('./DB_logic/wordPerSocketDB')
 
 
 router.get('/', function(req, res) {
     res.json({ message: 'API is Online!' })
+})
+
+router.route('/register-key-word').post(async function(req, res) {
+   const keyWord = req.body.keyWord
+
+   if(!keyWord)
+     return res.status(400).send("Bad request")
+
+   try{
+     wordPerSocketDB.registerKeyWord(keyWord)
+     return res.status(200).send("Success")
+   }catch(e){
+     return res.status(500).send("Server error")
+   }
+})
+
+
+router.route('/register-socket-by-keyword').post(async function(req, res) {
+   const keyWord = req.body.keyWord
+   const socket = req.body.socket
+
+   if(!keyWord || !socket)
+     return res.status(400).send("Bad request")
+
+   try{
+     wordPerSocketDB.addSocketByKeyWord(keyWord, socket)
+     return res.status(200).send("Success")
+   }catch(e){
+     return res.status(500).send("Server error")
+   }
 })
 
 
